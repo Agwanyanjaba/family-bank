833b631c98109cc3408455e39b106d991d7ed2a1
Agwanyanjaba <bonkewycliffe@yahoo.com>
2023-05-30 07:36:58 +0300

    configure server port

---
+++ src/main/java/com/familybank/FamilybankApplication.java
@@ -0,0 +1,13 @@
+package com.familybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilybankApplication.class, args);
+	}
+
+}
